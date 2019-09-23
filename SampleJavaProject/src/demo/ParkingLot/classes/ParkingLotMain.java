5fddbc023d27eef0fdbd73d0de975858b8abf24a
Mathur <Ramit.Mathur@fda.gov>
2019-09-23 18:01:42 -0400

    changes added for parkingLot

--- SampleJavaProject/src/demo/ParkingLot/classes/ParkingLotMain.java
+++ SampleJavaProject/src/demo/ParkingLot/classes/ParkingLotMain.java
@@ -1,4 +1,18 @@
 package demo.ParkingLot.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLotMain {
+    ParkingLotRec pLR = new ParkingLotRec();
+    public static void main(String args[]){
+
+        List<ParkingLotMain> parkinglotList = new ArrayList<ParkingLotMain>();
+    }
+
+    public List<ParkingLotMain> getStatusOfParking(){
+
+
+        return null;
+    }
 }
---
+++ SampleJavaProject/src/demo/ParkingLot/classes/ParkingLotRec.java
@@ -0,0 +1,22 @@
+package demo.ParkingLot.classes;
+
+public class ParkingLotRec {
+    public int parkingNumber;
+    public boolean parkingFlag;
+
+    public int getParkingNumber() {
+        return parkingNumber;
+    }
+
+    public void setParkingNumber(int parkingNumber) {
+        this.parkingNumber = parkingNumber;
+    }
+
+    public boolean isParkingFlag() {
+        return parkingFlag;
+    }
+
+    public void setParkingFlag(boolean parkingFlag) {
+        this.parkingFlag = parkingFlag;
+    }
+}
